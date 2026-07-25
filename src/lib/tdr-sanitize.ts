d5351f397310d00f65c29c2908d590a3146a5bc4
olla11 <olla.admi@gmail.com>
2026-07-25 17:45:25 +0100

    Update tdr-sanitize.ts

--- src/lib/tdr-sanitize.ts
+++ src/lib/tdr-sanitize.ts
@@ -5,8 +5,9 @@ import type { Chapitre } from './tdr'
 // souligné/lien/tableau) : on nettoie le HTML à l'écriture ET à l'affichage
 // PDF, pour empêcher toute injection de script par un collaborateur en révision.
 const OPTIONS: sanitizeHtml.IOptions = {
-  allowedTags: ['p', 'br', 'strong', 'b', 'em', 'i', 'u', 'a', 'ul', 'ol', 'li', 'table', 'thead', 'tbody', 'tr', 'th', 'td'],
-  allowedAttributes: { a: ['href', 'target', 'rel'], p: ['style'] },
+  allowedTags: ['p', 'br', 'strong', 'b', 'em', 'i', 'u', 'a', 'ul', 'ol', 'li', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'span'],
+  allowedAttributes: { a: ['href', 'target', 'rel'], p: ['style'], span: ['data-comment-id'] },
+  allowedClasses: { span: ['tdr-comment-highlight'] },
   allowedSchemes: ['http', 'https', 'mailto'],
   allowedStyles: {
     p: { 'text-align': [/^left$/, /^right$/, /^center$/, /^justify$/] },
